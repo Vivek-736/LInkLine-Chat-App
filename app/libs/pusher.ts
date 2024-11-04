6e64b9b79b0822b7e33b11f71d02a989be6a334b
Vivek <vivekousik8555@gmail.com>
2024-11-04 21:27:40 +0530

    next-public change

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -3,13 +3,13 @@ import PusherClient from 'pusher-js';
 
 export const pusherServer = new PusherServer({
     appId: process.env.PUSHER_APP_ID!,
-    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    key: process.env.PUSHER_APP_KEY!,
     secret: process.env.PUSHER_SECRET!,
     cluster: 'ap2',
     useTLS: true
 })
 
-export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+export const pusherClient = new PusherClient(process.env.PUSHER_APP_KEY!,
     {
         cluster: 'ap2'
     }
