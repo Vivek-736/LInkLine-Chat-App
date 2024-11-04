17069d06b319a36ce12e603bb758ca2e6e26cff5
Vivek <vivekousik8555@gmail.com>
2024-11-04 21:16:27 +0530

    relative img fix

--- app/components/AvatarGroup.tsx
+++ app/components/AvatarGroup.tsx
@@ -19,7 +19,7 @@ const AvatarGroup: React.FC<AvatarGroupProps> = ({ users = [] }) => {
         <div className='relative h-11 w-11'>
             {slicedUsers.map((user, index) => (
                 <div key={user.id} className={`absolute inline-block rounded-full overflow-hidden h-[21px] w-[21px] 4 ${positionMap[index as keyof typeof positionMap]}`}>
-                    <Image alt="Avatar" fill src={user?.image || 'images/user.png'}/>
+                    <Image alt="Avatar" fill src={user?.image || '/images/user.png'}/>
                 </div>
             ))}
         </div>
