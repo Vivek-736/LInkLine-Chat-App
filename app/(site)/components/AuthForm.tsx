5efac32e5cc81d27209fb908eda768212f193d6b
Vivek <vivekousik8555@gmail.com>
2025-01-08 17:31:12 +0530

    logo change

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -87,7 +87,7 @@ const AuthForm = () => {
 
   return (
     <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
-      <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+      <div className='bg-white px-4 py-8 drop-shadow-2xl sm:rounded-lg sm:px-10'>
 
         <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
           {variant === "REGISTER" && (
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import logo from "@/public/images/logo.png"
+import logo from "@/public/logo.jpg"
 import AuthForm from "./components/AuthForm"
 
 export default function Home() {
     return (
-        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-[#fefefe]">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <Image src={logo} width="58" height="58" alt="logo" className="mx-auto w-auto" />
+                <Image src={logo} width="70" height="70" alt="logo" className="mx-auto w-auto -my-7" />
                 <h2 className="mt-6 text-center text-3xl font-bold text-gray-900 tracking-tight">
                     <span className="bg-gradient-to-b from-[#6c14c5] to-[#29c6ee] bg-clip-text text-transparent">LogIn to your account!</span>
                 </h2>
